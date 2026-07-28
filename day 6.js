227007603a3ee442908abe2e0b01e699f70864a8
Adeel <adeel176wb@gmail.com>
2026-07-28 11:34:13 +0500

    Day 6: DOM manipulation, setTimeout, async/await complete

--- day 6.js
+++ day 6.js
@@ -1,3 +1,16 @@
 function changeText(){
     document.getElementById("title").innerHTML = "Welcome Adeel";
+    document.getElementById("title").style.color = "red";
 }
+console.log("program start")
+setTimeout(function() {
+    console.log("2 second baad ye print ho");
+}, 2000);
+console.log("Ye turant print hoga");
+async function getData(){
+    let result = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data= await result.json();
+    console.log(data);
+}
+
+getData();
