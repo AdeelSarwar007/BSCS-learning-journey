1ca8e1db286d4eb246cf457049c7d616284bdd82
Adeel <adeel176wb@gmail.com>
2026-08-03 09:53:08 +0500

    Week 2 Day 7: OOP Basics with Classes and Objects

---
+++ Week 2 Day 7.py
@@ -0,0 +1,20 @@
+class Student:
+    pass
+
+s1 = Student()
+s1.name = "Adeel"
+s1.marks = 90
+
+s2 = Student()
+s2.name = "Ali"
+s2.marks = 80
+
+print(s1.name, s1.marks)
+print(s2.name, s2.marks)
+
+
+    
+
+
+
+
