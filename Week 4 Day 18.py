f2b2c82448e8a1570019c7db3ac91e55db004471
Adeel <adeel176wb@gmail.com>
2026-08-06 13:04:19 +0500

    Week 4 Day 18: SQLite Database Basics

---
+++ Week 4 Day 18.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+id INTEGER PRIMARY KEY,
+name TEXT,
+marks INTEGER
+)
+""")
+
+conn.commit()
+
+print("Student Table Created Successfully")
