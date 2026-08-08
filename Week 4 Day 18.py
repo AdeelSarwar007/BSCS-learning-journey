f48ead0adb10e0006f51d3ca3423e77b5f3f1ace
Adeel <adeel176wb@gmail.com>
2026-08-08 12:34:56 +0500

    Day 20 SQLite CRUD Project

--- Week 4 Day 18.py
+++ Week 4 Day 18.py
@@ -1,17 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect("students.db")
+conn = sqlite3.connect("library.db")
 
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS students(
-id INTEGER PRIMARY KEY,
-name TEXT,
-marks INTEGER
+CREATE TABLE IF NOT EXISTS teachers(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    book TEXT
 )
 """)
 
 conn.commit()
 
-print("Student Table Created Successfully")
+print("Database Connected")
+print("Teachers Table Created Successfully")
+
+conn.close()
