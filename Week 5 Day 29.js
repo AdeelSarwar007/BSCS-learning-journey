413b6243a123032def3cc80f4532fa4a7f4a716f
Adeel <adeel176wb@gmail.com>
2026-08-18 10:23:31 +0500

    Day 29 JavaScript Introduction

---
+++ Week 5 Day 29.js
@@ -0,0 +1,11 @@
+let name = "Adeel";
+let age = 21;
+let city = "Vehari";
+let degree = "BSCS";
+let cgpa = 2.5;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Degree:", degree);
+console.log("CGPA:", cgpa);
