973f5b9185172cb7b8b0c4011cafe09daadfdf58
Adeel <adeel176wb@gmail.com>
2026-08-27 10:51:38 +0500

    Day 31 JavaScript Functions Practice

---
+++ Week 5 Day 31.js
@@ -0,0 +1,7 @@
+function studentInfo(name, age, degree) {
+    console.log("Name: " + name);
+    console.log("Age: " + age);
+    console.log("Degree: " + degree);
+}
+
+studentInfo("Adeel", 21, "BSCS");
