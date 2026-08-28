4005a441115f9223564141994258057d2f927abe
Adeel <adeel176wb@gmail.com>
2026-08-28 09:38:25 +0500

    Day 33 JavaScript Arrays Practice

---
+++ Month 2 Project.js
@@ -0,0 +1,12 @@
+let fruits = ["apple", "banana", "mango"];
+
+fruits.push("orange");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(fruits.length);
+
+let index = fruits.indexOf("banana");
+console.log(index);
---
+++ Week 5 Day 33.js
@@ -0,0 +1,12 @@
+let subjects = [
+    "Programming",
+    "Database",
+    "AI",
+    "Web"
+];
+console.log(subjects[0]);
+console.log(subjects);
+subjects.push("Tech");
+console.log(subjects);
+
+
