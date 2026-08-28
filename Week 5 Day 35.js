71d84e6578cc07b904d211439a627db94ab8b12a
Adeel <adeel176wb@gmail.com>
2026-08-28 10:41:35 +0500

    Day 35 JavaScript Objects Practice

---
+++ Week 5 Day 35.js
@@ -0,0 +1,13 @@
+let student = {
+    name: "Adeel",
+    age: 21,
+    city: "Vehari",
+    degree: "BSCS",
+    cgpa: 3.28
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("City:", student.city);
+console.log("Degree:", student.degree);
+console.log("CGPA:", student.cgpa);
