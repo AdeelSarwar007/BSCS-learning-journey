3bd3fe895feb703c5796c553be9c2abdabae0953
Adeel <adeel176wb@gmail.com>
2026-08-29 11:20:10 +0500

    Day 36 JavaScript Arrays of Objects Practice

--- Week 6 Day 36.js
+++ Week 6 Day 36.js
@@ -1,17 +1,27 @@
 let products = [
+
     {
-        name : "Laptop",
-        price : 120000
+        name: "Laptop",
+        price: 120000
     },
+
     {
         name: "Mobile",
         price: 50000
     },
+
     {
         name: "Headphones",
         price: 5000
     }
+
 ];
-for(let i=0;i<products.length;i++){
-    console.log(products[i])
+
+for(let i = 0; i < products.length; i++) {
+
+    console.log("Name:", products[i].name);
+
+    console.log("Price:", products[i].price);
+
+    console.log("----------------");
 }
